fff0fde8feea96dd75a6c60ae45ae5d8879ab8cd
Abram Hansen <hansenabram@gmail.com>
2023-10-07 13:13:23 -0600

    fixed needing to call by reference error

--- LevelUpdater.cpp
+++ LevelUpdater.cpp
@@ -16,34 +16,34 @@ int random(int min, int max)
    return num;
 }
 
-void LevelUpdater::levelUpdate(std::list<Bird*> birds) const {
+void LevelUpdater::levelUpdate(std::list<Bird*>& birds) const {
    this->timedSpawn(birds);
    this->emptySpawn(birds);
 }
 
-void Level1::timedSpawn(std::list<Bird*> birds) const {
+void Level1::timedSpawn(std::list<Bird*>& birds) const {
    if (random(0, 4 * 30) == 1)
       birds.push_back(new Standard(size, 7.0));
 }
 
-void Level1::emptySpawn(std::list<Bird*> birds) const {
+void Level1::emptySpawn(std::list<Bird*>& birds) const {
    if (birds.size() == 0 && random(0, 15) == 1)
       birds.push_back(new Standard(size, 7.0));
 }
 
-void Level2::timedSpawn(std::list<Bird*> birds) const {
+void Level2::timedSpawn(std::list<Bird*>& birds) const {
    if (random(0, 4 * 30) == 1)
       birds.push_back(new Standard(size, 5.0, 12));
    if (random(0, 3 * 30) == 1)
       birds.push_back(new Sinker(size));
 }
 
-void Level2::emptySpawn(std::list<Bird*> birds) const {
+void Level2::emptySpawn(std::list<Bird*>& birds) const {
    if (birds.size() == 0 && random(0, 15) == 1)
       birds.push_back(new Standard(size, 7.0, 12));
 }
 
-void Level3::timedSpawn(std::list<Bird*> birds) const {
+void Level3::timedSpawn(std::list<Bird*>& birds) const {
    if (random(0, 4 * 30) == 1)
       birds.push_back(new Standard(size, 5.0, 15));
    if (random(0, 4 * 30) == 1)
@@ -52,12 +52,12 @@ void Level3::timedSpawn(std::list<Bird*> birds) const {
       birds.push_back(new Floater(size));
 }
 
-void Level3::emptySpawn(std::list<Bird*> birds) const {
+void Level3::emptySpawn(std::list<Bird*>& birds) const {
    if (birds.size() == 0 && random(0, 15) == 1)
       birds.push_back(new Standard(size, 5.0, 15));
 }
 
-void Level4::timedSpawn(std::list<Bird*> birds) const {
+void Level4::timedSpawn(std::list<Bird*>& birds) const {
    if (random(0, 4 * 30) == 1)
       birds.push_back(new Standard(size, 4.0, 18));
    if (random(0, 4 * 30) == 1)
@@ -68,7 +68,7 @@ void Level4::timedSpawn(std::list<Bird*> birds) const {
       birds.push_back(new Crazy(size));
 }
 
-void Level4::emptySpawn(std::list<Bird*> birds) const {
+void Level4::emptySpawn(std::list<Bird*>& birds) const {
    if (birds.size() == 0 && random(0, 15) == 1)
       birds.push_back(new Standard(size, 4.0, 18));
 }
--- LevelUpdater.h
+++ LevelUpdater.h
@@ -8,37 +8,37 @@ class Time;
 
 class LevelUpdater {
 public:
-   void levelUpdate(std::list<Bird*> birds) const;
+   void levelUpdate(std::list<Bird*>& birds) const;
 
 protected:
-   virtual void timedSpawn(std::list<Bird*> birds) const = 0;
-   virtual void emptySpawn(std::list<Bird*> birds) const = 0;
+   virtual void timedSpawn(std::list<Bird*>& birds) const = 0;
+   virtual void emptySpawn(std::list<Bird*>& birds) const = 0;
 };
 
 class Level1 : public LevelUpdater {
 protected:
    int size = 30;
-   void timedSpawn(std::list<Bird*> birds) const override;
-   void emptySpawn(std::list<Bird*> birds) const override;
+   void timedSpawn(std::list<Bird*>& birds) const override;
+   void emptySpawn(std::list<Bird*>& birds) const override;
 };
 
 class Level2 :  public LevelUpdater {
 protected:
    int size = 25;
-   void timedSpawn(std::list<Bird*> birds) const override;
-   void emptySpawn(std::list<Bird*> birds) const override;
+   void timedSpawn(std::list<Bird*>& birds) const override;
+   void emptySpawn(std::list<Bird*>& birds) const override;
 };
 
 class Level3 : public LevelUpdater {
 protected:
    int size = 20;
-   void timedSpawn(std::list<Bird*> birds) const override;
-   void emptySpawn(std::list<Bird*> birds) const override;
+   void timedSpawn(std::list<Bird*>& birds) const override;
+   void emptySpawn(std::list<Bird*>& birds) const override;
 };
 
 class Level4 : public LevelUpdater {
 protected:
    int size = 15;
-   void timedSpawn(std::list<Bird*> birds) const override;
-   void emptySpawn(std::list<Bird*> birds) const override;
+   void timedSpawn(std::list<Bird*>& birds) const override;
+   void emptySpawn(std::list<Bird*>& birds) const override;
 };
